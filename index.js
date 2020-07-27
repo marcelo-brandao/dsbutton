d352f4e4d8d15db741343fe30dcb6c10dda7e061
unknown <marcelo.barbosa@medgrupo.com.br>
2020-07-27 13:26:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function dsbutton(options) {
+    let buttons = document.querySelectorAll('.dsbutton');
+
+    if (options.background_color === 'blue')
+        options.background_color = '#32d2f3'
+        else 
+        options.background_color = '#cccccc'
+
+        buttons.forEach(estilos => {
+            estilos.style.backgroundColor = `${options.background_color}`;
+            estilos.style.width = '100px';
+            estilos.style.height = '30px';
+            estilos.style.borderRadius = '50px';
+            estilos.style.color = '#ffffff';
+            estilos.style.textAlign = 'center';
+            estilos.style.lineHeight = '30px';
+        })
+}
+
+module.exports.dsbutton = dsbutton;
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Botao padrao MSPRO
+
+Botao que será usado para interações
+
+# Install
+
+`npm i dsbutton --save`
+
+```
+import {dsbutton} from 'dsbutton';
+
+dsbutton({
+    background_color: 'blue',
+})
+```
+
+## Options
+
+dsbutton é o botão padrão usado para interações no MSPRO e no momento apenas uma opção é customizavel: 
+
+- background color
+
+* *background_color* - _blue / cinza_ (Default é azul)
